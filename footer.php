c662af2501fb9d09b33a94ac2ec971b72d90037f
jecefi.com <165978209+JeffryCabalog@users.noreply.github.com>
2025-04-18 15:40:52 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="page-footer text-center font-small mt-4 wow fadeIn">
+<!--Copyright-->
+<div class="footer-copyright py-3">
+    © 2025 Copyright:
+    <a href="#"> Flappy </a>
+</div>
+<!--/.Copyright-->
+<!-- Button trigger modal -->
+
+</footer>
+<!--/.Footer-->
